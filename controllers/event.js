dbf4354f311bfd6a272e300ecaac219db1098869
Guy Sanchez <uni.ghsanchez>
2020-02-02 15:18:13 -0600

    Cambios

--- controllers/event.js
+++ controllers/event.js
@@ -9,14 +9,14 @@ var Event = require('../models/event');
 function newEvent(req, res){
     var event = new Event();
     var params = req.body;
-    var user = auth.ensureAuth(req.headers.authorization);
+    //var user = auth.ensureAuth(req.headers.authorization);
 
     event.name = params.name;
     event.link = params.link;
     event.image = 'null'
     event.datecreat = Date.now();
     event.dataedit = null;
-    // event.datedel = null;
+    event.datedel = null;
     // event.personcreat = user.firstname;
     // event.personedit = null;
     // event.persondel = null;
@@ -33,7 +33,42 @@ function newEvent(req, res){
         }
     });
 }
+function getEvent(req, res){
+    var enventId = req.params.id;
+    
+    Event.findById(enventId, (err, event)=>{
+        if(err){
+            res.status(500).send({message:'Error en la solicitud'});
+        }else{
+            if(!event){
+                res.status(404).send({message:'Evento no existe'});
+            }else{
+                res.status(200).send({event:event});
+            }
+        }
+    });
+}
+
+function getEventPubli(req, res){
+    var eventNm = req.params.id;
+
+    Event.findById(eventNm, (err, event)=>{
+        if(err){
+            res.status(500).send({message:'Error en la solicitud'});
+        }else{
+            if(!event){
+                res.status(404).send({message:'No existe el Evento'});
+            }else{
+                res.status(200).send({event:event});
+                console.log(event);
+                console.log(eventNm);
+            }
+        }
+    });
+}
 
 module.exports = {
-    newEvent
+    newEvent,
+    getEvent,
+    getEventPubli
 }
--- routes/event.js
+++ routes/event.js
@@ -8,6 +8,8 @@ var md_auth = require('../middlewares/authenticated');
 var multipart = require('connect-multiparty');
 var md_upload = multipart({uploadDir: './uploads/user'});
 
-api.post('/newEvent', md_auth.ensureAuth, EventController.newEvent)
+api.post('/newEvent', md_auth.ensureAuth, EventController.newEvent);
+api.get('/getEvent/:id', md_auth.ensureAuth, EventController.getEvent);
+api.get('/getEventPublic/:id', EventController.getEventPubli);
 
 module.exports = api;
