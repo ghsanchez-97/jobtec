f0bb41abb19b486f265f6528ce13ae27fd2b7fa1
Guy Sanchez <uni.ghsanchez>
2020-02-10 17:43:01 -0600

    Nuevas funciones

--- controllers/agend.js
+++ controllers/agend.js
@@ -13,7 +13,7 @@ function saveAgend(req, res){
     agend.description = params.description;
     agend.detail = params.detail;
     agend.date = params.date;
-    agend,datecreat = Date.now();
+    agend.datecreat = Date.now();
 
     agend.save((err, agendStored) =>{
         if(err){
@@ -29,7 +29,101 @@ function saveAgend(req, res){
     });
 
 }
+function getAgend(req, res){
+    var agendId = req.params.id;
 
-module.exports ={
-    saveAgend
+    Agend.findById(agendId, (err, agend) =>{
+        if(err){
+            res.status(500).send({message:'Error en la solicitud'});
+        }else{
+            if(!agend){
+                res.status(404).send({message:'Error no exite Agenda'});
+            }else{
+                res.status(200).send({agend});
+            }
+        }
+    });
+}
+function getAgendPublic(req, res){
+    var agendId = req.params.name;
+
+    Agend.findOne({name:agendId}, (err, agend) =>{
+        if(err){
+            res.status(500).send({message:'Error en la solicitud'});
+        }else{
+            if(!agend){
+                res.status(404).send({message:'Error no exite Agenda'});
+            }else{
+                res.status(200).send({agend});
+            }
+        }
+    });
+}
+function getsAgends(req, res){
+    var page = req.params.page;
+    var itemPerPage = 3;
+
+    Agend.find({$where: function(){
+        return (this.datedel == null);
+    }}).sort('name').paginate(page, itemPerPage, function(err, agend, total){
+        if(err){
+            res.status(500).send({message:'Error en la solicitud'});
+        }else{
+            if(!agend){
+                res.status(404).send({message:'Error no hay Agendas'});
+            }else{
+                return res.status(200).send({
+                    total:total,
+                    agend:agend,
+                });
+            }
+        }
+    });
+}
+function getsAgendsPublic(req, res){
+    var page = req.params.page;
+    var itemPerPage = 3;
+
+    Agend.find({$where: function(){
+        return (this.datedel == null);
+    }}).sort('name').paginate(page, itemPerPage, function(err, agend, total){
+        if(err){
+            res.status(500).send({message:'Error en la solicitud'});
+        }else{
+            if(!agend){
+                res.status(404).send({message:'Error no hay Agendas'});
+            }else{
+                return res.status(200).send({
+                    total:total,
+                    agend:agend,
+                });
+            }
+        }
+    });
+}
+function updateAgend(req, res){
+    var agendId = req.params.id;
+    var update = req.body;
+    update.dataedit = Date.now();
+
+    Agend.findByIdAndUpdate(agendId, update, (err, agendUpdate) =>{
+        if(err){
+            res.status(500).send({message:'Error en la solicitud'});
+        }else{
+            if(!agendUpdate){
+                res.status(404).send({message:'Error no se actualizo Agenda'});
+            }else{
+                res.status(200).send({agendUpdate:agendUpdate});
+            }
+        }
+    });
+}
+
+module.exports = {
+    saveAgend,
+    getAgend,
+    getAgendPublic,
+    getsAgends,
+    getsAgendsPublic,
+    updateAgend
 }
--- routes/agend.js
+++ routes/agend.js
@@ -6,5 +6,10 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.post('/newAgend', md_auth.ensureAuth, AgendController.saveAgend);
+api.get('/getAgend/:id', md_auth.ensureAuth, AgendController.getAgend);
+api.get('/getAgendPublic/:name', AgendController.getAgendPublic);
+api.get('/getsAgends/:page?', md_auth.ensureAuth, AgendController.getsAgends);
+api.get('/getsAgendsPublic/:page?', AgendController.getsAgendsPublic);
+api.put('/updateAgend/:id', md_auth.ensureAuth, AgendController.updateAgend);
 
 module.exports = api;
