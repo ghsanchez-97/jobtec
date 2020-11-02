531f83951246b1ae5fda3f7472a2652298405120
anakin25@live.com <anakin25@live.com>
2020-11-02 11:56:19 -0600

    Cambios en cors

--- apps.js
+++ apps.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyparse = require('body-parser');
 const https = require('https');
+const cors = require('cors')
 
 const app = express();
 
@@ -12,16 +13,16 @@ const agent_routes = require('./routes/agent');
 
 app.use(bodyparse.urlencoded({extended:false}));
 app.use(bodyparse.json());
-
+app.use(cors());
 //configure headers http
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 
 
 //rutes base
